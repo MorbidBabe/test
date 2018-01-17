e4a22b4969f6bd4731ee7be09b46788e2d59cd3b
Marquise <morbidbabe@live.com>
2018-01-16 23:26:32 -0500

    Encryption program

--- test/test.py
+++ test/test.py
@@ -1,1 +1,14 @@
-print("hello!")
+#!/bin/python3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 3
+newMessage = ''
+message = input('please enter a message:')
+for character in message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        newposition = (position + key) % 26 
+        newCharacter = alphabet [newposition]
+        newMessage += newCharacter
+    else:
+        newMessage += character
+print(newMessage)
